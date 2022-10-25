624118bf287155f88578377765caf6c06c8e05c8
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-10-25 10:51:24 -0300

    add route layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Route'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
